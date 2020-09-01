22372c20cd042d2cc0b2a4d75a0d6d26cfdd9f54
Karishmakarkii <karkikarishma143@gmail.com>
2020-09-01 15:34:57 +0545

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPIreMhR8X5Xs0Tf6y_wEiHQcWRiHjU8M",
+    authDomain: "tik-tok-clone-52a27.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-52a27.firebaseio.com",
+    projectId: "tik-tok-clone-52a27",
+    storageBucket: "tik-tok-clone-52a27.appspot.com",
+    messagingSenderId: "482767545175",
+    appId: "1:482767545175:web:8270980cfcf3bba008d412",
+    measurementId: "G-XMMW4C4T7N"
+  };
